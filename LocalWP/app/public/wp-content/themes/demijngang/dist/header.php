a5761c0c6fc6771ec9cc7fb166513f01738bcfc7
Ozzymops <justinmuris@gmail.com>
2025-11-28 14:37:34 +0100

    Updated theme, built dist

---
+++ LocalWP/app/public/wp-content/themes/demijngang/dist/header.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * The header for our theme
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ *
+ * @package Understrap
+ */
+
+// Exit if accessed directly.
+defined( 'ABSPATH' ) || exit;
+
+$bootstrap_version = get_theme_mod( 'understrap_bootstrap_version', 'bootstrap4' );
+$navbar_type       = get_theme_mod( 'understrap_navbar_type', 'collapse' );
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?> <?php understrap_body_attributes(); ?>>
+<?php do_action( 'wp_body_open' ); ?>
+<div class="site" id="page">
+
+	<!-- ******************* The Navbar Area ******************* -->
+	<div class="wrapper-header">
+		<header id="wrapper-navbar">
+			<div class="nav-background"></div>
+			<a class="skip-link <?php echo understrap_get_screen_reader_class( true ); ?>" href="#content">
+				<?php esc_html_e( 'Skip to content', 'understrap' ); ?>
+			</a>
+
+			<?php get_template_part( 'global-templates/navbar', $navbar_type . '-' . $bootstrap_version ); ?>
+
+		</header>
+	</div><!-- #wrapper-navbar -->
